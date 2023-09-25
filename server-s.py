d2ca3daa76eeb07aadf65fef8b2a6be211117d45
idamore043 <imore043@fiu.edu>
2023-09-24 22:22:01 -0400

    Re-work

--- server-s.py
+++ server-s.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
-
 import socket
 import signal
 import sys
+import time
 
-HOST = "0.0.0.0" # Standard loopback interface address (localhost)
-not_stopped = True
+HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
+NOT_STOPPED = True
 
-
-def handler (signum,frame):
-    not_stopped = False
+def handler(signum, frame):
+    global NOT_STOPPED
+    NOT_STOPPED = False
+    sys.exit()
 
 def main():
     if len(sys.argv) != 2:
@@ -27,28 +27,28 @@ def main():
         sys.stderr.write("ERROR: Invalid port number\n")
         sys.exit(1)
 
- #  signal.signal(signal.SIGQUIT, handler)
-    signal.signal(signal.SIGTERM, handler)
+    # signal.signal(signal.SIGQUIT, handler)
+    # signal.signal(signal.SIGTERM, handler)
     signal.signal(signal.SIGINT, handler)
+
     socket.setdefaulttimeout(10)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, server_port ))
+        s.bind((HOST, server_port))
         s.listen(10)
-
         try:
             conn, addr = s.accept()
             conn.send(b'accio\r\n')
             with conn:
-                while not_stopped:
+                while NOT_STOPPED:
                     data = conn.recv(1024)  # Receive data in chunks
                     total_bytes_received = len(data)
 
-                while data:
-                    data = conn.recv(1024)
-                    total_bytes_received += len(data)
+                    while data:
+                        data = conn.recv(1024)
+                        total_bytes_received += len(data)
 
-                print(total_bytes_received)
+                    print("Bytes Received:", total_bytes_received)
         except socket.timeout:
             sys.stderr.write("ERROR: Connection Timeout\n")
 
