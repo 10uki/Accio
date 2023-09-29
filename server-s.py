7612e9c68651396f1c90b0d646fa39d97a31bc11
idamore043 <imore043@fiu.edu>
2023-09-29 03:00:11 -0400

    Re-work, RENAMED.

--- server-s.py
+++ server-s.py
@@ -34,11 +34,11 @@ def main():
     signal.signal(signal.SIGINT, handler)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.bind((HOST, PORT))
-            s.listen(10)
-            while RUNNING:
+        while RUNNING:
+            try:
                 conn, addr = s.accept()
+                print(f"Connected by {addr}")
+                print("Sending accio")
                 conn.send(b'accio\r\n')
                 total_bytes_received = 0
 
@@ -48,11 +48,11 @@ def main():
                         if not data:
                             break
                         total_bytes_received += len(data)
-                        # print("Temp bytes: " + str(total_bytes_received))
                     print(str(total_bytes_received))
-
-        except socket.timeout:
-            sys.stderr.write("ERROR: Connection Timeout\n")
+            except socket.timeout:
+                sys.stderr.write("ERROR: Connection Timeout\n")
+            except OSError as e:
+                sys.stderr.write(f"ERROR: {e}\n")
 
     s.close()
     sys.exit()
