675bf6d7901493d6d4d1a156ba2e6bc1a60ab420
idamore043 <imore043@fiu.edu>
2023-10-07 16:32:57 -0400

    Moved server-s.py to server.py .

--- server.py
+++ server.py
@@ -1,6 +1,61 @@
 #!/usr/bin/env python3
 
+import socket
 import sys
 
-if __name__ == '__main__':
-    sys.stderr.write("server is not implemented yet\n")
+socket.setdefaulttimeout(10)
+
+HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
+
+def handle_client(conn, addr):
+    try:
+        total_bytes_received = 0
+        conn.send(b'accio\r\n')
+
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+                total_bytes_received += len(data)
+            print(total_bytes_received)
+
+    except socket.timeout:
+        sys.stderr.write("ERROR: Connection Timeout.\n")
+
+    except Exception as e:
+        sys.stderr.write(f"ERROR: {str(e)}\n")
+
+def main():
+    if len(sys.argv) != 2:
+        sys.stderr.write("ERROR: Usage - python3 server-s.py <PORT>\n")
+        sys.exit(1)
+
+    PORT = sys.argv[1]
+
+    try:
+        # A valid range for TCP port numbers (1-65535).
+        PORT = int(PORT)
+        if not 1 <= PORT <= 65535:
+            raise ValueError
+    except ValueError:
+        sys.stderr.write("ERROR: Invalid port number.\n")
+        sys.exit(1)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind((HOST, PORT))
+            s.listen(10)
+
+            while True:
+                try:
+                    conn, addr = s.accept()
+                    handle_client(conn, addr)
+                except socket.timeout:
+                    sys.stderr.write("ERROR: Connection Timeout.\n")
+        except OSError as e:
+            sys.stderr.write(f"ERROR: {str(e)}\n")
+
+if __name__ == "__main__":
+    main()
