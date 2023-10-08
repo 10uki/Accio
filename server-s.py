9d9f8e01932c419cd1561d2b1d51a2a1bfd00afe
idamore043 <imore043@fiu.edu>
2023-10-07 23:12:06 -0400

    Removed sendall method.

--- server-s.py
+++ server-s.py
@@ -3,7 +3,8 @@ import signal
 import sys
 import threading
 
-# Constants
+socket.setdefaulttimeout(10)
+
 HOST = "0.0.0.0"
 
 # Function to handle signals (SIGINT, SIGQUIT, SIGTERM)
@@ -11,27 +12,28 @@ def signal_handler(signum, frame):
     sys.exit(0)
 
 # Set signal handlers
-def setup_signal_handlers():
-    for sig in [signal.SIGINT, signal.SIGQUIT, signal.SIGTERM]:
-        signal.signal(sig, signal_handler)
+for sig in [signal.SIGINT, signal.SIGQUIT, signal.SIGTERM]:
+    signal.signal(sig, signal_handler)
 
 def handle_client(conn, addr):
     try:
         total_bytes_received = 0
         conn.send(b'accio\r\n')
 
-        with conn:
-            while True:
-                data = conn.recv(socket.SOL_SOCKET)
-                if not data:
-                    break
-                total_bytes_received += len(data)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            # Send data in smaller chunks
+            bytes_sent = 0
+            while bytes_sent < len(data):
+                sent = conn.send(data[bytes_sent:])
+                if sent == 0:
+                    raise RuntimeError("Socket connection broken")
+                bytes_sent += sent
+            total_bytes_received += len(data)
 
-                sent = 0
-                while sent < len(data):
-                    sent += conn.send(data[sent:])
-
-                print(f"Received {total_bytes_received} bytes")  # Print bytes received
+        print(total_bytes_received)
 
     except socket.timeout:
         sys.stderr.write("ERROR: Connection Timeout.\n")
@@ -39,11 +41,12 @@ def handle_client(conn, addr):
     except Exception as e:
         sys.stderr.write(f"ERROR: {str(e)}\n")
 
-
 def establish_connection(PORT):
     try:
+        # Create a socket object using IPv4 and TCP protocol
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(10)
+        # Set timeout for socket operations
+        s.settimeout(10)
         s.bind((HOST, PORT))
         s.listen(10)
         return s
@@ -66,8 +69,6 @@ def main():
     except ValueError:
         sys.stderr.write("ERROR: Invalid port number.\n")
         sys.exit(1)
-
-    setup_signal_handlers()
 
     try:
         s = establish_connection(PORT)
