318cdae3e78d70f13afca45c74aa62707ab496bc
idamore043 <imore043@fiu.edu>
2023-10-08 17:53:30 -0400

    Used random module to add artificial delays and packet drop.

--- test.py
+++ test.py
@@ -5,43 +5,24 @@ import threading
 
 socket.setdefaulttimeout(10)
 
-# Constants
 HOST = "0.0.0.0"
-HEADER_SIZE= len(b'accio\r\n') * 2
 
-# Create a global lock.
+# Create a global lock
 file_lock = threading.Lock()
 
-# Function to handle signals (SIGINT, SIGQUIT, SIGTERM).
+# Function to handle signals (SIGINT, SIGQUIT, SIGTERM)
 def signal_handler(signum, frame):
     sys.exit(0)
 
-# Set signal handlers.
+# Set signal handlers
 for sig in [signal.SIGINT, signal.SIGQUIT, signal.SIGTERM]:
     signal.signal(sig, signal_handler)
-
-def establish_connection(host, port):
-    try:
-        # Create a socket object using IPv4 and TCP protocol.
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Set timeout for socket operations
-        s.settimeout(10)
-        try:
-            # Bind the socket to the provided host and port.
-            s.bind((host, port))
-            # Listen for incoming connections with a backlog of 10.
-            s.listen(10)
-        except socket.error:
-            sys.stderr.write(f"ERROR: Connection failed.\n")
-            sys.exit(1)
-        return s
-    except Exception as e:
-        sys.stderr.write(f"ERROR: {str(e)}\n")
-        sys.exit(1)
 
 def handle_client(conn, addr):
     try:
         conn.send(b'accio\r\n')
+
+        total_bytes_received = 0
 
         confirmation = conn.recv(1024)
         if confirmation != b'confirm-accio\r\n':
@@ -49,22 +30,22 @@ def handle_client(conn, addr):
 
         conn.send(b'accio\r\n')
 
-        bytes_received = 0
-
         with file_lock:  # Acquire the lock before file operations
-            with open('received_file.bin', 'wb') as file:
+            with open(f'received_file.bin', 'wb') as file:
                 while True:
                     data = conn.recv(1024)
                     if not data:
                         break
                     file.write(data)
-                    bytes_received += len(data)
+                    total_bytes_received += len(data)
 
-        print(bytes_received - HEADER_SIZE)
+        print(total_bytes_received)
+
+    except socket.timeout:
+        sys.stderr.write("ERROR: Connection Timeout.\n")
 
     except Exception as e:
         sys.stderr.write(f"ERROR: {str(e)}\n")
-
     finally:
         conn.close()
 
@@ -84,17 +65,26 @@ def main():
         sys.stderr.write("ERROR: Invalid port number.\n")
         sys.exit(1)
 
-    with establish_connection(HOST, PORT) as s:
+    try:
+        # Create a socket object using IPv4 and TCP protocol
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Set timeout for socket operations
+        s.settimeout(10)
+        # Bind the socket to the provided host and port
+        s.bind((HOST, PORT))
+        # Listen for incoming connections with a backlog of 10
+        s.listen(10)
+
         while True:
             try:
                 conn, addr = s.accept()
-                # Start a new thread to handle the client.
+                # Start a new thread to handle the client
                 client_thread = threading.Thread(target=handle_client, args=(conn, addr))
                 client_thread.start()
             except socket.timeout:
                 sys.stderr.write("ERROR: Connection Timeout.\n")
-            except Exception as e:
-                sys.stderr.write(f"ERROR: {str(e)}\n")
+    except Exception as e:
+        sys.stderr.write(f"ERROR: {str(e)}\n")
 
 if __name__ == "__main__":
     main()
