67d12d04014066521c651fd12b72ba84161adc27
idamore043 <imore043@fiu.edu>
2023-10-10 00:12:40 -0400

    Cleaned code, added comments, and re-visions.

--- client.py
+++ client.py
@@ -1,28 +1,42 @@
 import socket
 import sys
 
+# Set default timeout to ten seconds.
 socket.setdefaulttimeout(10)
 
+# Set max chunks to 10000.
 MAX_CHUNKS = 10000
 
+
 def receive_command(s, command):
+    # Initialize an empty buffer to store received data.
     received_data = b""
     try:
+        # Continue receiving data until the received_data ends with the specified command.
         while not received_data.endswith(command):
+            # Receive one byte of data from the socket.
             chunk = s.recv(1)
+            # If no data is received, raise a ConnectionError and print error message.
             if not chunk:
                 sys.stderr.write("ERROR: Server disconnected.\n")
                 raise ConnectionError
+            # Append the received chunk to the data buffer.
             received_data += chunk
     except socket.timeout:
+        # Handle a socket timeout exception raising a TimeoutError and print error message.
         sys.stderr.write("ERROR: Connection Timeout.\n")
         raise TimeoutError
+    # Return the received data, which should now include the specified command.
     return received_data
+
 
 def send_file(s, FILE):
     # Open the specified file for reading in binary mode
     try:
         with open(FILE, 'rb') as file:
+            # While the size of the chunks is equal to the maximum permitted allowed
+            # chunks send the length of the data, if the amount set is equal to zero
+            # output RuntimeError code.
             while True:
                 chunk = file.read(MAX_CHUNKS)
                 if not chunk:
@@ -34,9 +48,11 @@ def send_file(s, FILE):
                         sys.stderr.write("ERROR: Socket connection broken.\n")
                         raise RuntimeError
                     total_sent += sent
+    # Otherwise, if the file is not found, FileNotFoundError.
     except FileNotFoundError:
         sys.stderr.write("ERROR: File not found.\n")
         sys.exit(1)
+
 
 def establish_connection(HOST, PORT):
     try:
@@ -56,15 +72,21 @@ def establish_connection(HOST, PORT):
         sys.stderr.write(f"ERROR: {str(e)}\n")
         raise
 
+
 def client():
+    # Checks if the script is provided with the correct number of command-line
+    # arguments. If not, display ValueError code and exits.
     if len(sys.argv) != 4:
         sys.stderr.write("ERROR: Usage - python3 client.py <HOSTNAME-OR-IP> <PORT> <FILENAME>\n")
         sys.exit(1)
 
+    # Extracts the host, port, and file name from the command-line arguments.
     HOST = sys.argv[1]
     PORT = sys.argv[2]
     FILE = sys.argv[3]
 
+    # Validate value of port is a valid integer between 1 and 65535.
+    # If not, display ValueError code and exits.
     try:
         PORT = int(PORT)
         if not (1 <= PORT <= 65535):
@@ -74,22 +96,32 @@ def client():
         sys.stderr.write("ERROR: Port number must be an integer.\n")
         sys.exit(1)
 
+    # Call establish_connection function with specified HOST and PORT.
     s = establish_connection(HOST, PORT)
 
+    # Send and receive "accio" commands from the server using the receive_command function.
     try:
         receive_command(s, b'accio\r\n')
         s.send(b'confirm-accio\r\n')
         receive_command(s, b'accio\r\n')
-        # s.send(b'confirm-accio-again\r\n\r\n')
+        s.send(b'confirm-accio-again\r\n\r\n')
 
+        # Send the file to the server.
         send_file(s, FILE)
-        print("File transfer successful")
+        # Confirm file transfer successful.
+        print("File transfer successful.")
+    # In the case of network disconnection, display TimeoutError and exit.
     except TimeoutError:
         sys.stderr.write("ERROR: Connection Timeout.\n")
+        sys.exit(1)
+    # Handle any other exceptions that may occur during file transfer.
+    except Exception as e:
+        sys.stderr.write(f"ERROR: File transfer failed: {str(e)}.\n")
         sys.exit(1)
 
     s.close()
     sys.exit(0)
+
 
 if __name__ == '__main__':
     client()
--- server.py
+++ server.py
@@ -5,89 +5,113 @@ import signal
 import os
 import time
 
+# Set default timeout to ten seconds.
 socket.setdefaulttimeout(10)
 
+# Set as '0.0.0.0' as per instructions.
 HOST = '0.0.0.0'
+
 
 # Function to handle signals (SIGINT, SIGQUIT, SIGTERM)
 def signal_handler(signum, frame):
     sys.exit(0)
 
+
 # Set signal handlers.
 for sig in [signal.SIGINT, signal.SIGQUIT, signal.SIGTERM]:
     signal.signal(sig, signal_handler)
 
-def establish_connection(host, port):
+
+def establish_connection(PORT):
     try:
         # Create a socket object using IPv4 and TCP protocol.
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Set timeout for socket operations
+        # Set timeout for socket operations.
         s.settimeout(10)
         try:
             # Bind the socket to the provided host and port.
-            s.bind((host, port))
+            s.bind((HOST, PORT))
             # Listen for incoming connections with a backlog of 10.
             s.listen(10)
         except socket.error:
-            sys.stderr.write(f"ERROR: Connection failed.\n")
+            sys.stderr.write("ERROR: Connection failed.\n")
             sys.exit(1)
         return s
     except Exception as e:
-        sys.stderr.write(f"ERROR: {str(e)}\n")
+        sys.stderr.write(f"ERROR: {str(e)}.\n")
         sys.exit(1)
+
+
+# Function to validate expected confirmation.
+def receive_confirmation(conn, expected_confirmation):
+    confirmation = conn.recv(len(expected_confirmation))
+    if confirmation != expected_confirmation:
+        sys.stderr.write("ERROR: Invalid confirmation.\n")
+        raise RuntimeError
+
 
 def handle_client(conn, file_dir, file_number):
     try:
-        # Set the initial data time.
+        # Sets the initial timestamp when the function is first called.
         data_time = time.time()
 
         # Send the first accio command to the client.
         conn.send(b'accio\r\n')
 
-        # Receive confirmation from the client.
-        confirmation = conn.recv(1024)
-        if confirmation != b'confirm-accio\r\n':
-            raise RuntimeError("Error: Invalid confirmation.")
+        # Receive and validate the first confirmation.
+        receive_confirmation(conn, b'confirm-accio\r\n')
 
         # Send the second accio command to the client.
         conn.send(b'accio\r\n')
 
-        # Receive and save the binary data sent by the client.
+        # Receive and validate the second confirmation.
+        receive_confirmation(conn, b'confirm-accio-again\r\n\r\n')
+
+        # Initialize an empty buffer to store received data.
         data = b""
+        # Reset data_time to track the time since the last data reception.
         data_time = time.time()
         while True:
+            # Receive data chunks of 1024 bytes of data from the socket otherwise exit loop.
             chunk = conn.recv(1024)
             if not chunk:
                 break
+            # Append the received chunk to the data buffer.
             data += chunk
 
             # Reset the timeout timer whenever data is received.
             data_time = time.time()
-
+    # Handle any other exceptions that may occur during data transfer.
     except Exception as e:
         sys.stderr.write(f"ERROR: {str(e)}.\n")
         data = b"ERROR"
 
-        # Check if no data received for more than 10 seconds
+    # Check if no data received for more than 10 seconds.
     if time.time() - data_time > 10:
         sys.stderr.write("ERROR: Connection Timeout.\n")
         data = b"ERROR"
 
-        # Save the data to a file with a sequential name using the global lock.
+    # Write and save the data to a file with a sequential name and close connection.
     file_path = os.path.join(file_dir, f"{file_number}.file")
     with open(file_path, 'wb') as f:
         f.write(data)
 
     conn.close()
 
+
 def main():
+    # Checks if the script is provided with the correct number of command-line
+    # arguments. If not, display ValueError code and exits.
     if len(sys.argv) != 3:
         sys.stderr.write("ERROR: Usage - python3 server.py <PORT> <FILE-DIR>\n")
         sys.exit(1)
 
+    # Extracts the port and file directory from the command-line arguments.
     PORT = int(sys.argv[1])
     file_dir = sys.argv[2]
 
+    # Validate value of port is a valid integer between 1 and 65535.
+    # If not, display ValueError code and exits.
     if not 1 <= PORT <= 65535:
         sys.stderr.write("ERROR: Invalid port number.\n")
         sys.exit(1)
@@ -95,19 +119,25 @@ def main():
     # Create the file directory if it does not exist
     os.makedirs(file_dir, exist_ok=True)
 
-    with establish_connection(HOST, PORT) as s:
+    with establish_connection(PORT) as s:
+        # Initialize a counter for the received files.
         file_number = 1
+        # Continuously accept incoming client connections.
         while True:
             try:
+                # Accept a new client connection and retrieve the client's address.
                 conn, addr = s.accept()
                 # Start a new thread to handle the client.
                 client_thread = threading.Thread(target=handle_client, args=(conn, file_dir, file_number))
                 client_thread.start()
+                # Increment file number size.
                 file_number += 1
+            # Handle a socket timeout exception and any other exceptions.
             except socket.timeout:
                 sys.stderr.write("ERROR: Connection Timeout.\n")
             except Exception as e:
-                sys.stderr.write(f"ERROR: {str(e)}\n")
+                sys.stderr.write(f"ERROR: {str(e)}.\n")
+
 
 if __name__ == "__main__":
     main()
